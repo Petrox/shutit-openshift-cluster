1786a7eedf35e80781858627123b6ee5f9c06d0a
Ian Miell <ian.miell@gmail.com>
2017-02-17 14:49:22 +0000

    add etcd machines

---
+++ tests/etcd_migration/machines.py
@@ -0,0 +1,8 @@
+machines = {}
+machines.update({'master1':{'fqdn':'master1.vagrant.test','is_node':True,'region':'infra'}})
+machines.update({'master2':{'fqdn':'master2.vagrant.test','is_node':True,'region':'infra'}})
+machines.update({'master3':{'fqdn':'master3.vagrant.test','is_node':True,'region':'infra'}})
+machines.update({'etcd1':{'fqdn':'etcd1.vagrant.test','is_node':False,'region':''}})
+machines.update({'etcd2':{'fqdn':'etcd2.vagrant.test','is_node':False,'region':''}})
+machines.update({'etcd3':{'fqdn':'etcd3.vagrant.test','is_node':False,'region':''}})
+machines.update({'node1':{'fqdn':'node1.vagrant.test','is_node':True,'region':'user'}})
