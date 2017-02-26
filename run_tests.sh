2c9a75a3ad28d1c28de96632180597850b3a87df
Ian Miell <ian.miell@gmail.com>
2017-02-26 14:40:45 +0000

    test corrected

--- run_tests.sh
+++ run_tests.sh
@@ -91,7 +91,7 @@ $SHUTIT build \
 	-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster test_config_dir                       multi_node_basic \
 	-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster ose_version                           1.2.1-1.el7 \
 	-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster ose_major_version                     1.2 \
-	-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster cookbook_branch                       master} \
+	-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster cookbook_branch                       master \
 	-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster chef_yum_cookbook_version             3.6.1 \
 	-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster chef_iptables_cookbook_version        1.0.0 \
 	-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster chef_selinux_policy_cookbook_version  0.7.2 \
