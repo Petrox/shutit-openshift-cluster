7100860210da7ce68c3a4f64fe95b77a26d89540
Ian Miell <ian.miell@gmail.com>
2017-02-27 14:19:11 +0000

    kitchen one at a time

--- run_tests.sh
+++ run_tests.sh
@@ -105,6 +105,9 @@ cd ..
 git clone --recursive https://github.com/IshentRas/cookbook-openshift3
 cd cookbook-openshift3
 git checkout ${GIT_BRANCH##origin/}
-kitchen converge
-kitchen verify
-kitchen destroy
+for i in $(kitchen test -b)
+do
+	kitchen converge $i
+	kitchen verify $i
+	kitchen destroy $i
+done
