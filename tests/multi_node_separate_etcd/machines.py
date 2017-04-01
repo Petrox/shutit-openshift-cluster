32794af62c3ab4f626cc6cb845f9477168bbc651
Ian Miell <ian.miell@gmail.com>
2017-04-01 07:47:19 +0100

    tests

---
+++ tests/multi_node_separate_etcd/machines.py
@@ -0,0 +1,7 @@
+machines = {}
+machines.update({'master1':{'fqdn':'master1.vagrant.test','is_node':True,'region':'infra'}})
+machines.update({'master2':{'fqdn':'master2.vagrant.test','is_node':True,'region':'infra'}})
+machines.update({'etcd1':{'fqdn':'etcd1.vagrant.test','is_node':False,'region':'none'}})
+machines.update({'etcd2':{'fqdn':'etcd2.vagrant.test','is_node':False,'region':'none'}})
+machines.update({'etcd3':{'fqdn':'etcd3.vagrant.test','is_node':False,'region':'none'}})
+machines.update({'node1':{'fqdn':'node1.vagrant.test','is_node':True,'region':'user'}})
