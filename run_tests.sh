1d248c93175d01035073b0a5833cdad95605fa3f
Ian Miell <ian.miell@gmail.com>
2017-05-17 18:38:17 +0100

    json

--- run_tests.sh
+++ run_tests.sh
@@ -29,58 +29,58 @@ chef_selinux_policy_cookbook_version="latest"
 chef_compat_resource_cookbook_version="latest"
 inject_compat_resource="false"
 
-if [[ ${QUICK:-0} = '1' ]]
-then
-	echo 'LOG: RUNNING QUICK MODE'
-	$SHUTIT build \
-		-l debug \
-		--echo -d bash \
-		-m shutit-library/vagrant:shutit-library/virtualbox \
-		-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster test_config_dir                       test_single_master_embedded_etcd \
-		-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster ose_version                           1.4.1-1.el7 \
-		-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster ose_major_version                     1.4 \
-		-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster cookbook_branch                       ${cookbook_branch} \
-		-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster chef_yum_cookbook_version             ${chef_yum_cookbook_version} \
-		-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster chef_iptables_cookbook_version        ${chef_iptables_cookbook_version} \
-		-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster chef_selinux_policy_cookbook_version  ${chef_selinux_policy_cookbook_version} \
-		-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster chef_compat_resource_cookbook_version ${chef_compat_resource_cookbook_version} \
-		-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster chef_version                          12.16.42-1 \
-		-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster inject_compat_resource                ${inject_compat_resource} \
-		"$@"
-	./destroy_vms.sh
-else
-	for ose_major_version in ${OSE_VERSIONS}
-	do
-		for test_dir in $(cd cluster_configs && find * -type d | grep ^test && cd - > /dev/null)
-		do
-			if [[ $ose_major_version == '1.4' ]]
-			then
-			        ose_version="1.4.1-1.el7"
-			elif [[ $ose_major_version == '1.3' ]]
-			then
-			        ose_version="1.3.3-1.el7"
-			fi
-	
-			echo "LOG: RUNNING test_dir:${test_dir} ose_version:${ose_version} ose_major_version:${ose_major_version} cookbook_branch:${cookbook_branch}"
-			$SHUTIT build \
-				-l debug \
-				--echo -d bash \
-				-m shutit-library/vagrant:shutit-library/virtualbox \
-				-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster test_config_dir                       ${test_dir} \
-				-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster ose_version                           ${ose_version} \
-				-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster ose_major_version                     ${ose_major_version} \
-				-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster cookbook_branch                       ${cookbook_branch} \
-				-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster chef_yum_cookbook_version             ${chef_yum_cookbook_version} \
-				-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster chef_iptables_cookbook_version        ${chef_iptables_cookbook_version} \
-				-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster chef_selinux_policy_cookbook_version  ${chef_selinux_policy_cookbook_version} \
-				-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster chef_compat_resource_cookbook_version ${chef_compat_resource_cookbook_version} \
-				-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster chef_version                          12.16.42-1 \
-				-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster inject_compat_resource                ${inject_compat_resource} \
-				"$@"
-			./destroy_vms.sh
-		done
-	done
-fi
+#if [[ ${QUICK:-0} = '1' ]]
+#then
+#	echo 'LOG: RUNNING QUICK MODE'
+#	$SHUTIT build \
+#		-l debug \
+#		--echo -d bash \
+#		-m shutit-library/vagrant:shutit-library/virtualbox \
+#		-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster test_config_dir                       test_single_master_embedded_etcd \
+#		-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster ose_version                           1.4.1-1.el7 \
+#		-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster ose_major_version                     1.4 \
+#		-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster cookbook_branch                       ${cookbook_branch} \
+#		-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster chef_yum_cookbook_version             ${chef_yum_cookbook_version} \
+#		-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster chef_iptables_cookbook_version        ${chef_iptables_cookbook_version} \
+#		-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster chef_selinux_policy_cookbook_version  ${chef_selinux_policy_cookbook_version} \
+#		-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster chef_compat_resource_cookbook_version ${chef_compat_resource_cookbook_version} \
+#		-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster chef_version                          12.16.42-1 \
+#		-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster inject_compat_resource                ${inject_compat_resource} \
+#		"$@"
+#	./destroy_vms.sh
+#else
+#	for ose_major_version in ${OSE_VERSIONS}
+#	do
+#		for test_dir in $(cd cluster_configs && find * -type d | grep ^test && cd - > /dev/null)
+#		do
+#			if [[ $ose_major_version == '1.4' ]]
+#			then
+#			        ose_version="1.4.1-1.el7"
+#			elif [[ $ose_major_version == '1.3' ]]
+#			then
+#			        ose_version="1.3.3-1.el7"
+#			fi
+#	
+#			echo "LOG: RUNNING test_dir:${test_dir} ose_version:${ose_version} ose_major_version:${ose_major_version} cookbook_branch:${cookbook_branch}"
+#			$SHUTIT build \
+#				-l debug \
+#				--echo -d bash \
+#				-m shutit-library/vagrant:shutit-library/virtualbox \
+#				-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster test_config_dir                       ${test_dir} \
+#				-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster ose_version                           ${ose_version} \
+#				-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster ose_major_version                     ${ose_major_version} \
+#				-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster cookbook_branch                       ${cookbook_branch} \
+#				-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster chef_yum_cookbook_version             ${chef_yum_cookbook_version} \
+#				-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster chef_iptables_cookbook_version        ${chef_iptables_cookbook_version} \
+#				-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster chef_selinux_policy_cookbook_version  ${chef_selinux_policy_cookbook_version} \
+#				-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster chef_compat_resource_cookbook_version ${chef_compat_resource_cookbook_version} \
+#				-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster chef_version                          12.16.42-1 \
+#				-s tk.shutit.shutit_openshift_cluster.shutit_openshift_cluster inject_compat_resource                ${inject_compat_resource} \
+#				"$@"
+#			./destroy_vms.sh
+#		done
+#	done
+#fi
 
 # $WORK-specific
 $SHUTIT build \
