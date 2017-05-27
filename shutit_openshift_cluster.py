7004a5783efdcd55cc38f3f38a7134140818e756
Ian Miell <ian.miell@gmail.com>
2017-05-27 10:03:05 +0100

    fasterfster

--- shutit_openshift_cluster.py
+++ shutit_openshift_cluster.py
@@ -48,66 +48,94 @@ class shutit_openshift_cluster(ShutItModule):
 			ip = shutit.send_and_get_output('''vagrant landrush ls 2> /dev/null | grep -w ^''' + test_config_module.machines[machine]['fqdn'] + ''' | awk '{print $2}' ''')
 			test_config_module.machines.get(machine).update({'ip':ip})
 		for machine in sorted(test_config_module.machines.keys()):
-			shutit_sessions.update({machine:shutit.create_session('bash')}
+			shutit_sessions.update({machine:shutit.create_session('bash')})
 			shutit_session = shutit_sessions[machine]
-			shutit_session('cd ' + run_dir + '/' + module_name)
-			shutit_session('vagrant ssh ' + machine)
-			print(shutit_session.send_and_get_output('hostname')
-		shutit.pause_point('here')
-		for machine in test_config_module.machines.keys():
-			shutit.login(command='vagrant ssh ' + machine)
-			shutit.login(command='sudo su - ')
-			shutit.send('''sed -i 's/enabled=1/enabled=0/' /etc/yum/pluginconf.d/fastestmirror.conf''',note='Switch off fastest mirror - it gives me nothing but grief (looooong waits')
-			shutit.send('rm -fr /var/cache/yum/*')
-			shutit.send('yum clean all')
-			shutit.install('net-tools')
-			shutit.install('git')
-			# Allow logins via ssh between machines
-			shutit.send('echo root:origin | /usr/sbin/chpasswd',note='set root password')
-			shutit.send('rpm -i https://packages.chef.io/stable/el/7/chef-' + shutit.cfg[self.module_id]['chef_version'] + '.el7.x86_64.rpm',note='install chef')
-			shutit.send('mkdir -p /root/chef-solo-example /root/chef-solo-example/cookbooks /root/chef-solo-example/environments /root/chef-solo-example/logs',note='Create chef folders')
-			shutit.send('cd /root/chef-solo-example/cookbooks')
-			shutit.send('git clone -b ' + shutit.cfg[self.module_id]['cookbook_branch'] + ' https://github.com/IshentRas/cookbook-openshift3',note='Clone chef repo')
-			shutit.send('cd cookbook-openshift3 && git checkout ' + shutit.cfg[self.module_id]['cookbook_branch'] + ' && cd -',note='Checkout branch')
+			shutit_session.send('cd ' + run_dir + '/' + module_name)
+			shutit_session.login(command='vagrant ssh ' + machine)
+			shutit_session.login(command='sudo su - ')
+
+		for machine in sorted(test_config_module.machines.keys()):
+			shutit_session = shutit_sessions[machine]
+			shutit_session.send('echo root:origin | /usr/sbin/chpasswd',note='set root password')
+			shutit_session.send('''sed -i 's/enabled=1/enabled=0/' /etc/yum/pluginconf.d/fastestmirror.conf''',note='Switch off fastest mirror - it gives me nothing but grief (looooong waits')
+			shutit_session.send('rm -fr /var/cache/yum/*')
+			shutit_session.send('yum clean all')
+			#shutit_session.install('net-tools')
+			shutit_session.install('git',background=True,wait=False,block_other_commands=False)
+
+		for machine in sorted(test_config_module.machines.keys()):
+			shutit_session = shutit_sessions[machine]
+			shutit_session.wait()
+
+		for machine in sorted(test_config_module.machines.keys()):
+			shutit_session = shutit_sessions[machine]
+			shutit_session.send('mkdir -p /root/chef-solo-example /root/chef-solo-example/cookbooks /root/chef-solo-example/environments /root/chef-solo-example/logs',note='Create chef folders')
+			shutit_session.send('cd /root/chef-solo-example/cookbooks')
+			shutit_session.send('git clone -b ' + shutit.cfg[self.module_id]['cookbook_branch'] + ' https://github.com/IshentRas/cookbook-openshift3',note='Clone chef repo',background=True,wait=False,block_other_commands=False)
+
+		for machine in sorted(test_config_module.machines.keys()):
+			shutit_session = shutit_sessions[machine]
+			shutit_session.wait()
+
+		for machine in sorted(test_config_module.machines.keys()):
+			shutit_session = shutit_sessions[machine]
+			shutit_session.send('rpm -i https://packages.chef.io/stable/el/7/chef-' + shutit.cfg[self.module_id]['chef_version'] + '.el7.x86_64.rpm',note='install chef',background=True,wait=False,block_other_commands=False)
+			shutit_session.send('cd /root/chef-solo-example/cookbooks/cookbook-openshift3')
+			shutit_session.send('git checkout ' + shutit.cfg[self.module_id]['cookbook_branch'] + ' && cd -',note='Checkout branch',background=True,wait=False,block_other_commands=False)
 			# Test json validity in github code
-			shutit.send(r"""find . | grep json$ | sed 's/.*/echo \0 \&\& cat \0 | python -m json.tool > \/dev\/null/'  | sh""")
+			shutit_session.send(r"""find . | grep json$ | sed 's/.*/echo \0 \&\& cat \0 | python -m json.tool > \/dev\/null/'  | sh""",background=True,wait=False,block_other_commands=False)
 			if shutit.cfg[self.module_id]['inject_compat_resource']:
-				shutit.send("""echo "depends 'compat_resource'" >> cookbook-openshift3/metadata.rb""")
+				shutit_session.send("""echo "depends 'compat_resource'" >> cookbook-openshift3/metadata.rb""",background=True,wait=False,block_other_commands=False)
+
+		for machine in sorted(test_config_module.machines.keys()):
+			shutit_session = shutit_sessions[machine]
+			shutit_session.wait()
+
+		for machine in sorted(test_config_module.machines.keys()):
+			shutit_session = shutit_sessions[machine]
 			# Filthy hack to 'override' the node['ipaddress'] value
-			ip_addr = shutit.send_and_get_output("""ip -4 addr show dev eth1 | grep inet | awk '{print $2}' | awk -F/ '{print $1}'""")
-			shutit.send('''sed -i 's/#{node..ipaddress..}/''' + ip_addr + '''/g' /root/chef-solo-example/cookbooks/cookbook-openshift3/attributes/default.rb''')
-			shutit.send("""sed -i "s/node..ipaddress../'""" + ip_addr + """'/g" /root/chef-solo-example/cookbooks/cookbook-openshift3/attributes/default.rb""")
+			ip_addr = shutit_session.send_and_get_output("""ip -4 addr show dev eth1 | grep inet | awk '{print $2}' | awk -F/ '{print $1}'""")
+			shutit_session.send('''sed -i 's/#{node..ipaddress..}/''' + ip_addr + '''/g' /root/chef-solo-example/cookbooks/cookbook-openshift3/attributes/default.rb''',background=True,wait=False,block_other_commands=False)
+			shutit_session.send("""sed -i "s/node..ipaddress../'""" + ip_addr + """'/g" /root/chef-solo-example/cookbooks/cookbook-openshift3/attributes/default.rb""",background=True,wait=False,block_other_commands=False)
+
+		for machine in sorted(test_config_module.machines.keys()):
+			shutit_session = shutit_sessions[machine]
+			shutit_session.wait()
+
+		for machine in sorted(test_config_module.machines.keys()):
+			shutit_session = shutit_sessions[machine]
+			shutit_session.send('cd /root/chef-solo-example/cookbooks')
 			if shutit.cfg[self.module_id]['chef_iptables_cookbook_version'] == 'latest':
-				shutit.send('curl -L https://supermarket.chef.io/cookbooks/iptables/download | tar -zxvf -',note='Get cookbook dependencies')
+				shutit_session.send('curl -L https://supermarket.chef.io/cookbooks/iptables/download | tar -zxvf -',note='Get cookbook dependencies',background=True,wait=False,block_other_commands=False)
 			else:
-				shutit.send('curl -L https://supermarket.chef.io/cookbooks/iptables/versions/'+ shutit.cfg[self.module_id]['chef_iptables_cookbook_version'] + '/download | tar -zxvf -',note='Get cookbook dependencies')
+				shutit_session.send('curl -L https://supermarket.chef.io/cookbooks/iptables/versions/'+ shutit.cfg[self.module_id]['chef_iptables_cookbook_version'] + '/download | tar -zxvf -',note='Get cookbook dependencies',background=True,wait=False,block_other_commands=False)
 			if shutit.cfg[self.module_id]['chef_yum_cookbook_version'] == 'latest':
-				shutit.send('curl -L https://supermarket.chef.io/cookbooks/yum/download | tar -zxvf -',note='Get cookbook dependencies')
+				shutit_session.send('curl -L https://supermarket.chef.io/cookbooks/yum/download | tar -zxvf -',note='Get cookbook dependencies',background=True,wait=False,block_other_commands=False)
 			else:
-				shutit.send('curl -L https://supermarket.chef.io/cookbooks/yum/versions/'+ shutit.cfg[self.module_id]['chef_yum_cookbook_version'] + '/download | tar -zxvf -',note='Get cookbook dependencies')
+				shutit_session.send('curl -L https://supermarket.chef.io/cookbooks/yum/versions/'+ shutit.cfg[self.module_id]['chef_yum_cookbook_version'] + '/download | tar -zxvf -',note='Get cookbook dependencies',background=True,wait=False,block_other_commands=False)
 			if shutit.cfg[self.module_id]['chef_selinux_policy_cookbook_version'] == 'latest':
-				shutit.send('curl -L https://supermarket.chef.io/cookbooks/selinux_policy/download | tar -zxvf -',note='Get cookbook dependencies')
+				shutit_session.send('curl -L https://supermarket.chef.io/cookbooks/selinux_policy/download | tar -zxvf -',note='Get cookbook dependencies',background=True,wait=False,block_other_commands=False)
 			else:
-				shutit.send('curl -L https://supermarket.chef.io/cookbooks/selinux_policy/versions/'+ shutit.cfg[self.module_id]['chef_selinux_policy_cookbook_version'] + '/download | tar -zxvf -',note='Get cookbook dependencies')
+				shutit_session.send('curl -L https://supermarket.chef.io/cookbooks/selinux_policy/versions/'+ shutit.cfg[self.module_id]['chef_selinux_policy_cookbook_version'] + '/download | tar -zxvf -',note='Get cookbook dependencies',background=True,wait=False,block_other_commands=False)
 			if shutit.cfg[self.module_id]['chef_compat_resource_cookbook_version'] == 'latest':
-				shutit.send('curl -L https://supermarket.chef.io/cookbooks/compat_resource/download | tar -zxvf -',note='Get cookbook dependencies')
+				shutit_session.send('curl -L https://supermarket.chef.io/cookbooks/compat_resource/download | tar -zxvf -',note='Get cookbook dependencies',background=True,wait=False,block_other_commands=False)
 			else:
-				shutit.send('curl -L https://supermarket.chef.io/cookbooks/compat_resource/versions/'+ shutit.cfg[self.module_id]['chef_compat_resource_cookbook_version'] + '/download | tar -zxvf -',note='Get cookbook dependencies')
+				shutit_session.send('curl -L https://supermarket.chef.io/cookbooks/compat_resource/versions/'+ shutit.cfg[self.module_id]['chef_compat_resource_cookbook_version'] + '/download | tar -zxvf -',note='Get cookbook dependencies',background=True,wait=False,block_other_commands=False)
+
+		for machine in sorted(test_config_module.machines.keys()):
+			shutit_session = shutit_sessions[machine]
+			shutit_session.wait()
+
+		for machine in sorted(test_config_module.machines.keys()):
+			shutit_session = shutit_sessions[machine]
 			# Create solo.rb
 			template = jinja2.Template(file(self_dir + '/cluster_configs/' + shutit.cfg[self.module_id]['test_config_dir'] + '/solo.rb').read())
-			shutit.send_file('/root/chef-solo-example/solo.rb',str(template.render()),note='Create solo.rb file')
+			shutit_session.send_file('/root/chef-solo-example/solo.rb',str(template.render()),note='Create solo.rb file')
 			# Create environment file
 			template = jinja2.Template(file(self_dir + '/cluster_configs/' + shutit.cfg[self.module_id]['test_config_dir'] + '/environment.json').read())
-			shutit.send_file('/root/chef-solo-example/environments/ocp-cluster-environment.json',str(template.render(test_config_module=test_config_module,cfg=shutit.cfg[self.module_id])),note='Create environment file')
-			shutit.logout()
-			shutit.logout()
+			shutit_session.send_file('/root/chef-solo-example/environments/ocp-cluster-environment.json',str(template.render(test_config_module=test_config_module,cfg=shutit.cfg[self.module_id])),note='Create environment file')
+			shutit_session.send('echo "*/2 * * * * chef-solo --environment ocp-cluster-environment -o recipe[cookbook-openshift3] -c ~/chef-solo-example/solo.rb >> /root/chef-solo-example/logs/chef.log 2>&1" | crontab',note='set up crontab on ' + machine)
 
-		for machine in test_config_module.machines.keys():
-			shutit.login(command='vagrant ssh ' + machine)
-			shutit.login(command='sudo su - ')
-			shutit.send('echo "*/5 * * * * chef-solo --environment ocp-cluster-environment -o recipe[cookbook-openshift3] -c ~/chef-solo-example/solo.rb >> /root/chef-solo-example/logs/chef.log 2>&1" | crontab',note='set up crontab on ' + machine)	
-			shutit.logout()
-			shutit.logout()
 	
 		# CHECKS
 		# 1) CHECK NODES COME UP	
@@ -117,56 +145,53 @@ class shutit_openshift_cluster(ShutItModule):
 		for machine in test_config_module.machines.keys():
 			if test_config_module.machines[machine]['is_node']:
 				shutit.send_until('oc get nodes',machine + '.* Ready.*',cadence=60,note='Wait until oc get all returns OK')
-				
 		shutit.logout()
 		shutit.logout()
-		for machine in test_config_module.machines.keys():
-			shutit.login(command='vagrant ssh ' + machine)
-			shutit.login(command='sudo su -',password='vagrant')
+
+		# CONFIGURE DOCKER TO WORK
+		for machine in sorted(test_config_module.machines.keys()):
+			shutit_session = shutit_sessions[machine]
 			# Workaround for docker networking issues + landrush.
-			shutit.install('docker')
-			shutit.insert_text('Environment=GODEBUG=netdns=cgo','/lib/systemd/system/docker.service',pattern='.Service.')
-			shutit.send('mkdir -p /etc/docker',note='Create the docker config folder')
+			shutit_session.install('docker')
+			shutit_session.insert_text('Environment=GODEBUG=netdns=cgo','/lib/systemd/system/docker.service',pattern='.Service.')
+			shutit_session.send('mkdir -p /etc/docker',note='Create the docker config folder')
 			# The containers running in the pods take their dns setting from the docker daemon. Add the default kubernetes service ip to the list so that items can be updated.
 			# Ref: IWT-3895
-			shutit.send_file('/etc/docker/daemon.json',"""{
+			shutit_sessions.send_file('/etc/docker/daemon.json',"""{
   "dns": ["8.8.8.8"]
 }""",note='Use the google dns server rather than the vagrant one. Change to the value you want if this does not work, eg if google dns is blocked.')
 			shutit.send('systemctl daemon-reload && systemctl restart docker')
-			shutit.logout()
-			shutit.logout()
-		# CHECK APPS
-		shutit.login(command='vagrant ssh master1')
-		shutit.login(command='sudo su - ')
-		# Test json validity in json on server
-		shutit.send(r"""find / | grep json$ | sed 's/.*/echo \0 \&\& cat \0 | python -m json.tool > \/dev\/null/'  | sh""")
-		shutit.send_until('oc get pods | grep ^router- | grep -v deploy','.*Running.*',cadence=30)
-		shutit.send_until('oc get pods | grep ^docker-registry- | grep -v deploy','.*Running.*',cadence=30)
 
-		shutit.send('oc new-app -e=MYSQL_ROOT_PASSWORD=root mysql')
+		# CHECK APPS ON MASTER1
+		shutit_session = shutit_sessions['master1']
+		# Test json validity in json on server
+		shutit_session.send(r"""find / | grep json$ | sed 's/.*/echo \0 \&\& cat \0 | python -m json.tool > \/dev\/null/'  | sh""")
+		shutit_session.send_until('oc get pods | grep ^router- | grep -v deploy','.*Running.*',cadence=30)
+		shutit_session.send_until('oc get pods | grep ^docker-registry- | grep -v deploy','.*Running.*',cadence=30)
+		shutit_session.send('oc new-app -e=MYSQL_ROOT_PASSWORD=root mysql')
 		while True:
-			status = shutit.send_and_get_output("""oc get pods | grep ^mysql- | grep -v deploy | awk '{print $3}'""")
+			status = shutit_session.send_and_get_output("""oc get pods | grep ^mysql- | grep -v deploy | awk '{print $3}'""")
 			if status == 'Running':
 				break
 			elif status == 'Error':
-				shutit.send('oc deploy mysql --retry')
+				shutit_session.send('oc deploy mysql --retry')
 			elif status == 'ImagePullBackOff':
-				shutit.send('oc deploy mysql --cancel')
-				shutit.send('sleep 15')
-				shutit.send('oc deploy mysql --retry')
-			shutit.send('oc get all | grep mysql')
-			shutit.send('sleep 15')
+				shutit_session.send('oc deploy mysql --cancel')
+				shutit_session.send('sleep 15')
+				shutit_session.send('oc deploy mysql --retry')
+			shutit_session.send('oc get all | grep mysql')
+			shutit_session.send('sleep 15')
 
-		# Check version is as expected
-		shutit.send_and_get_output('oc version')
-			
-		shutit.pause_point('')
-		# TODO: exec and check hosts google.com and kubernetes.default.svc.cluster.local 
+		# Check version is as expected TODO
+		# TODO: exec and check hosts google.com and kubernetes.default.svc.cluster.local
+		shutit_session.send_and_get_output('oc version')
+		shutit_session.send("""/bin/bash -c 'set -xe ; for ip in $(oc get endpoints kubernetes -n default -o jsonpath="{.subsets[*].addresses[*].ip}"); do echo curl --fail -s -o/dev/null --cacert /etc/origin/node/ca.crt https://${ip}:8443 ; done'""")
 
 		# See: IshentRas/cookbook-openshif3 #119
-		shutit.send("""/bin/bash -c 'set -xe ; for ip in $(oc get endpoints kubernetes -n default -o jsonpath="{.subsets[*].addresses[*].ip}"); do echo curl --fail -s -o/dev/null --cacert /etc/origin/node/ca.crt https://${ip}:8443 ; done'""")
-		shutit.logout()
-		shutit.logout()
+		for machine in sorted(test_config_module.machines.keys()):
+			shutit_session = shutit_sessions[machine]
+			shutit_session.logout()
+			shutit_session.logout()
 		return True
 
 
