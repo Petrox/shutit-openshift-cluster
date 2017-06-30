18a9427364739dc0dddf4b2bbf12d46a0ce85aa3
Ian Miell <ian.miell@gmail.com>
2017-06-30 16:27:17 +0100

    test_single_master_embedded_etcd_cert_server/

---
+++ cluster_configs/test_single_master_embedded_etcd_cert_server/machines.py
@@ -0,0 +1,5 @@
+machines = {}
+machines.update({'master1':{'fqdn':'master1.vagrant.test','is_node':False,'region':'infra'}})
+machines.update({'node1':{'fqdn':'node1.vagrant.test','is_node':True,'region':'user'}})
+machines.update({'node2':{'fqdn':'node2.vagrant.test','is_node':True,'region':'user'}})
+machines.update({'certserver':{'fqdn':'certserver.vagrant.test','is_node':False,'region':'infra'}})
